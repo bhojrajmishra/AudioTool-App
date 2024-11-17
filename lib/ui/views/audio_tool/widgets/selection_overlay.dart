90f915a23c7e73df936c87e9d5354f233a149ceb
bhojrajmishra <bhojrajmishra4@gmail.com>
2024-11-17 11:15:54 +0545

    fix: optimize the code dividing widget of audiowavform selectionoverlay

--- lib/ui/views/audio_tool/widgets/selection_overlay.dart
+++ lib/ui/views/audio_tool/widgets/selection_overlay.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
 
+class SelectionOverlay extends StatelessWidget {
+  final double selectionStart;
+  final double selectionWidth;
+  final BoxConstraints constraints;
+
+  const SelectionOverlay({
+    super.key,
+    required this.selectionStart,
+    required this.selectionWidth,
+    required this.constraints,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      left: selectionStart * constraints.maxWidth,
+      width: selectionWidth * constraints.maxWidth,
+      top: 0,
+      bottom: 0,
+      child: Stack(
+        children: [
+          Container(color: Colors.red.withOpacity(0.3)),
+          _buildSelectionHandle(left: true),
+          _buildSelectionHandle(left: false),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSelectionHandle({required bool left}) {
+    return Positioned(
+      left: left ? 0 : null,
+      right: left ? null : 0,
+      top: 0,
+      bottom: 0,
+      child: Container(
+        width: 4,
+        color: Colors.red,
+      ),
+    );
+  }
+}
